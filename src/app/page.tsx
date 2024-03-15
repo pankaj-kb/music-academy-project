d90af59b00f2ac6463a63cf74f906af1be515489
pankaj-kb <pankajb2601@gmail.com>
2024-03-15 14:05:27 +0530

    adding testimonial cards and grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/Testimonials-Cards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main
-      className="min-h-screen bg-black/[0.96] antialiased 
-    bg-grid-white/[0.2]"
+      className="min-h-screen bg-white/[0.96] dark:bg-black/[0.96] antialiased bg-dot-black/[0.2]
+    dark:bg-dot-white/[0.2]"
     >
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
+      <MusicSchoolTestimonials />
     </main>
   );
 }
